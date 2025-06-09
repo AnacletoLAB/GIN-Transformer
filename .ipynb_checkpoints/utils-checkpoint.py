30d59fa5b8be1087afd8c99718f71020a9d396a1
NicoRota-0 <mnicolini@topone.anacleto.di.unimi.it>
2025-06-09 15:14:04 +0000

    Remove cached __pycache__ and ipynb checkpoints from repo

--- .ipynb_checkpoints/utils-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import random, numpy as np, torch
-
-def set_seed(seed: int = 7):
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-
